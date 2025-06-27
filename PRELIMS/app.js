22dbb5f34b1e4cca5afe3b3b9bf5ef9e4bd59f0c
PrincessLacanlale <lacanlaleprincess28@gmail.com>
2025-06-27 13:13:01 +0800

    Add files via upload

---
+++ PRELIMS/app.js
@@ -0,0 +1,11 @@
+var myLogModule = require('./PRELIMS/utility/log.js');
+
+myLogModule.warning('Warning mode not found. .');
+
+myLogModule.error('Error: Node encountered an error. .');
+
+myLogModule.info('Mode running. .');
+
+// var msg= require('./PRELIMS/utility/log.js');
+// console.log(msg);
+
---
+++ PRELIMS/utility/message.js
@@ -0,0 +1,4 @@
+module.exports = "Hello World"
+
+// var msg= require('./PRELIMS/utility/log.js');
+// console.log(msg);

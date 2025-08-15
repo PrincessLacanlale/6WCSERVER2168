38f8088b637574dcc960671f0e7f4f7de6c2438a
PrincessLacanlale <lacanlaleprincess28@gmail.com>
2025-08-15 12:02:52 +0800

    Add files via upload

---
+++ midterm/express-api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/data', (req,res) => {
+    res.json({ message: 'This is JSON data', time: new Date() 
+    });
+});
+
+app.get('/greet', (req,res) => {
+    const name = req.query.name || 'Guest';
+    res.json({ 
+        message: `Hello, ${name}!` ,
+        date: new Date(),
+        info: 'Same resoruce used with personalized using query parameters. '
+    });
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server running at http://localhost:${PORT}`);
+})
